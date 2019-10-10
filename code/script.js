53775b5c5fd3e946dea0c9292b203b0cb526af81
nasimmhn <nasim.mahzoun@gmail.com>
2019-10-10 23:12:50 +0200

    Added FAQ section with javascript

--- code/script.js
+++ code/script.js
@@ -1,9 +1,21 @@
 // A function that adds and remove the class "active" on the section you click on.
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
-function toggle() {
-  this.classList.toggle("active")
-}
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("section1").onclick = toggleAnswer
+document.getElementById("section2").onclick = toggleAnswer
+document.getElementById("section3").onclick = toggleAnswer
+
+function toggleAnswer() {
+  this.classList.toggle("open")
+  let plusButton = this.children[0]
+
+  if (this.classList.contains("open")) {
+    plusButton.innerHTML = "-"
+  } else {
+    plusButton.innerHTML = "+"
+  }
+  
+}
+
