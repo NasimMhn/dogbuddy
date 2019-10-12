0d94e5c87819a3deab7e11438886db5d0736aa48
nasimmhn <nasim.mahzoun@gmail.com>
2019-10-12 14:25:05 +0200

    fix the button position, added focus and box shadow in inputs

--- code/script.js
+++ code/script.js
@@ -1,7 +1,3 @@
-// A function that adds and remove the class "active" on the section you click on.
-// We haven't really talked about ´this´ yet, but we will... ;)
-// console.log(this) to see how it works
-
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggleAnswer
 document.getElementById("section2").onclick = toggleAnswer
